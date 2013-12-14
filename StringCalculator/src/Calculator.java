b1dd7b503b493520cf5829335f2f2ed392de9548
SarahGabriel <s_gab@hotmail.fr>
2013-12-14 21:11:07 +0200

    GREEN

--- StringCalculator/src/Calculator.java
+++ StringCalculator/src/Calculator.java
@@ -1,22 +1,38 @@
 
 public class Calculator
 {
+	private String delimiter = ",|\n";
+
 	int Add(String number)
 	{
-		String[] numbers = number.split(",|\n");
-
+		String[] numbers = number.split(delimiter);
+		int result =0;
 		if(number.isEmpty())
 		{
 			return 0;
 		}
-		else return sumNumbers(numbers);
+		else 
+		{
+			if(number.startsWith("//") && number.charAt(3)=='\n')
+			{
+				delimiter+="|";
+				delimiter+= number.charAt(2);
+				numbers = number.split(delimiter);
+				for(int i=2; i<numbers.length; i++)
+				{
+					result +=Integer.parseInt(numbers[i]);
+				}
+				return result;
+			}
+			else return sumNumbers(numbers);
+		}
 	}
 
 	private int stringToInt(String input)
 	{
 		return Integer.parseInt(input);
 	}
-	
+
 	private int sumNumbers(String[] numbers)
 	{
 		int number = 0;
